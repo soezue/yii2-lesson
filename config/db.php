45c07ba85c75718830a0f90a9ffa16beac26b3cb
webdev <salih_oeztuerk@webdev-service.com>
2017-05-27 12:51:25 +0200

    config params-test

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2-lesson',
     'username' => 'yii',
-    'password' => '123',
+    'password' => '---',
     'charset' => 'utf8',
 ];
