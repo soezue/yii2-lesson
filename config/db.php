baf56a4d8a7c04432eb43818cafdc5affffcd57a
webdev <salih_oeztuerk@webdev-service.com>
2017-05-27 12:42:46 +0200

    config params

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2-lession',
+    'dsn' => 'mysql:host=localhost;dbname=yii2-lesson',
     'username' => 'yii33',
     'password' => '123',
     'charset' => 'utf8',
